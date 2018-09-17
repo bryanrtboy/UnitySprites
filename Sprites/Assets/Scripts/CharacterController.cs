d5d4f06f865510a55a6292211d63849462de8426
bryanrtboy <bryan@bryanleister.com>
2018-09-17 11:29:44 -0600

    Imported Assets
    Imported Assets and created a stub script for character control

---
+++ Sprites/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+
+    public Animator m_animator;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            //Do Something 
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            //Do something else
+        }
+        else
+        {
+            //Do some default thing...
+        }
+
+    }
+}
